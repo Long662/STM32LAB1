496429a0fa449d8303b2664a8aab9b878ea67036
DESKTOP-UHU2R84\HP WORK <ntlong662@gmail.com>
2024-09-22 23:09:24 +0700

    Lab1_Ex6 to 10 Init

---
+++ Lab1_Src/Core/Src/Lab1_Lib/LAb1_Ex6to10.c
@@ -0,0 +1,11 @@
+/*
+ * LAb1_Ex6to10.c
+ *
+ *  Created on: Sep 22, 2024
+ *      Author: HP WORK
+ */
+
+#include "Lab1_Ex6to10.h"
+
+
+
---
+++ Lab1_Src/Core/Src/Lab1_Lib/Lab1_Ex6to10.h
@@ -0,0 +1,17 @@
+/*
+ * Lab1_Ex6to10.h
+ *
+ *  Created on: Sep 22, 2024
+ *      Author: HP WORK
+ */
+
+#ifndef SRC_LAB1_LIB_LAB1_EX6TO10_H_
+#define SRC_LAB1_LIB_LAB1_EX6TO10_H_
+
+#include "main.h"
+
+// Excercise 6
+void Lab1_Ex6_Init();
+void Lab1_Ex6_Run();
+
+#endif /* SRC_LAB1_LIB_LAB1_EX6TO10_H_ */
